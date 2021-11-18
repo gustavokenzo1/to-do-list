173c58487dc8fb643b067f7b198bef412789281f
Gustavo Kenzo <gustavokenzo314@gmail.com>
2021-11-17 22:53:54 -0300

    definitivo (???)

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -23,7 +23,7 @@ function Todo({ todos, completeTodo, removeTodo, updateTodo}) {
 
     return todos.map((todo, index) => (
         <div className={todo.isComplete ? 'todo-row complete' : 'todo-row'} key={index}>
-            <div key={todo.id} onClick={() => completeTodo(todo.id)}>
+            <div className='todo-task' key={todo.id} onClick={() => completeTodo(todo.id)}>
                 {todo.text}
             </div>
             <div className='icons'>
